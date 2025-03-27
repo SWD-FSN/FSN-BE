f9801af903f70bb679c7e15e3255afddc181eb5a
Phuchtq <taikhoanhoconl123@gmail.com>
2025-03-27 23:48:07 +0700

    Upload: search objects

--- controller/search_object.go
+++ controller/search_object.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	actiontype "social_network/constant/action_type"
 	"social_network/dto"
 	"social_network/service"
 	"social_network/util"
@@ -15,11 +16,12 @@ func GetObjectsByKeyword(ctx *gin.Context) {
 		return
 	}
 
-	var nullId = ""
+	var userId = ctx.Param("userId")
 	var keyword = ctx.Param("keyword")
 
 	util.ProcessResponse(dto.APIResponse{
-		Data1:   service.GetObjectsByKeyword(nullId, keyword, ctx),
-		Context: ctx,
+		Data1:    service.GetObjectsByKeyword(userId, keyword, ctx),
+		Context:  ctx,
+		PostType: actiontype.Non_post,
 	})
 }
